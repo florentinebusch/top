516c8fa7f7d712e3c642ff96e3e35a723a1713bb
Florentine Busch <florentine.busch@student.uibk.ac.at>
2025-03-24 16:41:56 +0100

    Karte mit SIN KOO ausgestattet

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var map = L.map('map').setView([-45.317222, 166.988333], 11);
+var map = L.map('map').setView([1.283333, 103.833333], 11);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
-        var marker = L.marker([-45.317222, 166.988333]).addTo(map);
+        var marker = L.marker([1.283333, 103.833333]).addTo(map);
         marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
